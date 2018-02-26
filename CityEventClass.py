fdff810fc55eb2be0c49027eff2b7f51d3dd31a3
Jakub Łowicki <jakublowicki@lowicz.local>
2018-02-26 21:04:36 +0100

    psorowe poprawki

--- CityEventClass.py
+++ CityEventClass.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import re
-
-BLUE_COLOR_TAG = "\x1B[34;40m"
-BOLD_TAG = "\x1B[1m"
-CLOSE_TAG = "\x1B[0m"
+from datetime import datetime
 
 
 class CityEvent(object):
-    def __init__(self, event_date, event_description):
-        prog = re.compile('^[0-9]{4}-[0-9]{2}-[0-9]{2}$')
-        if prog.match(event_date):
-            self.event_date = event_date
+    def __init__(self, event_start_date, event_end_date, event_description):
+            self.event_start_date = datetime.strptime(event_start_date,
+                                                      '%Y-%m-%d')
+            self.event_end_date = datetime.strptime(event_end_date, '%Y-%m-%d')
             self.event_description = event_description
-        else:
-            raise RuntimeError('Date must be in YYYY-MM-DD format')
 
     def description_search(self, pattern):
         return pattern.lower() in self.event_description.lower()
 
+    def date_search(self, event_date):
+        if self.event_start_date <= event_date <= self.event_end_date:
+            return True
+        else:
+            return False
+
     def print_event(self):
-        print BLUE_COLOR_TAG + BOLD_TAG + "Event Date: " \
-            + CLOSE_TAG + str(self.event_date)
-        print BLUE_COLOR_TAG + BOLD_TAG + "Event Description: " \
-            + CLOSE_TAG + str(self.event_description)
+        print("Event Start Date: " + self.event_start_date.strftime('%Y-%m-%d')
+              + " Event End Date: " + self.event_end_date.strftime('%Y-%m-%d'))
+        print("Event Description: " + str(self.event_description))
--- main.py
+++ main.py
@@ -4,8 +4,8 @@
 import xml.etree.ElementTree as ET
 import re
 import sys
-import getopt
 import requests
+from datetime import datetime
 from CityEventClass import CityEvent
 from requests.packages.urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
@@ -15,8 +15,8 @@ BOLD_TAG = "\x1B[1m"
 CLOSE_TAG = "\x1B[0m"
 XML_PATH = "http://bip.poznan.pl/api-xml/bip/news/-,c,8380/"
 HINT_TEXT = BOLD_TAG + "usage: " + CLOSE_TAG + \
-            "python main.py -d <date in format YYYY-MM-DD>" \
-            " -s <street (at least 4 characters)>"
+            "Type date in format YYYY-MM-DD" \
+            " or street (at least 4 characters)>"
 ERROR_TEXT = RED_COLOR_TAG + BOLD_TAG + "error: " + CLOSE_TAG
 
 
@@ -24,7 +24,7 @@ def xml_download(XML_PATH):
     # Download XML Element from PATH
     # Returns Element object or False if something goes wron
 
-    print "Downloading xml file..."
+    print("Downloading xml file...")
     s = requests.Session()
     retries = Retry(total=5,
                     backoff_factor=0.1,
@@ -33,23 +33,25 @@ def xml_download(XML_PATH):
     try:
         r = s.get(XML_PATH)
     except requests.ConnectionError:
-        print ERROR_TEXT + "Check your internet connection"
+        print(ERROR_TEXT + "Check your internet connection")
         return False
     if r.status_code == 200:
         try:
             return ET.fromstring(r.content)
         except ET.ParseError:
-            print ERROR_TEXT + "Wrong XML object, check your XML_PATH variable"
+            print(ERROR_TEXT +
+                  "Wrong XML object, check your XML_PATH variable")
             return False
     else:
-        print ERROR_TEXT + \
-            "Something went wrong... http_status = " + str(r.status_code)
+        print(ERROR_TEXT +
+              "Something went wrong... http_status = " + str(r.status_code))
 
 
 def xml_to_cityevents(element_object):
     # Read Element object trough and get data from:
-    #   1. <nw_to_date>
-    #   2. <nw_text>
+    #   1. <nw_from_date>
+    #   2. <nw_to_date>
+    #   3. <nw_text>
     # Returns list of objects CityEvent
 
     city_events_list = []
@@ -58,10 +60,13 @@ def xml_to_cityevents(element_object):
                                         .find("items")
 
     for announcement in announcements_items:
-        event_date = announcement.find("nw_to_date").text
-        event_description = announcement.find("nw_text").text.encode('utf-8')
-        event_description = re.sub('<.*?>', '', event_description)
-        city_events_list.append(CityEvent(event_date, event_description))
+        event_start_date = announcement.find("nw_from_date").text
+        event_end_date = announcement.find("nw_to_date").text
+        event_description = announcement.find("nw_text").text
+        event_description = re.sub('<.*?>', '', str(event_description))
+        city_events_list.append(CityEvent(event_start_date,
+                                          event_end_date,
+                                          event_description))
 
     return city_events_list
 
@@ -72,14 +77,10 @@ def search_events(city_events_list, event_date, event_desc_pattern):
 
     if event_desc_pattern is None and event_date is not None:
         return [x for x in city_events_list
-                if x.event_date == event_date]
+                if x.date_search(event_date)]
     elif event_desc_pattern is not None and event_date is None:
         return [x for x in city_events_list
                 if x.description_search(event_desc_pattern)]
-    else:
-        return [x for x in city_events_list
-                if (x.description_search(event_desc_pattern)
-                    and x.event_date == event_date)]
 
 
 def street_validator(street):
@@ -92,42 +93,32 @@ def street_validator(street):
 
 def date_validator(date):
 
-    prog = re.compile('^[0-9]{4}-[0-9]{2}-[0-9]{2}$')
-    if date is None or prog.match(date):
-        return True
-    else:
+    try:
+        return datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
         return False
 
 
-def main(argv):
+def main():
     # Main function
-
-    if len(argv) == 0:
-        print HINT_TEXT
-        sys.exit()
 
     date = None
     street = None
 
-    try:
-        opts, args = getopt.getopt(argv, "d:s:")
-    except getopt.GetoptError:
-        print HINT_TEXT
+    prog = re.compile('^[0-9]{4}.*')
+
+    input_value = input('Type street or date <YYYY-MM-DD>: ')
+
+    if prog.match(input_value) and date_validator(input_value) is False:
+        print(HINT_TEXT)
         sys.exit()
-    for opt, arg in opts:
-        if opt == '-d':
-            date = arg
-        elif opt == '-s':
-            street = arg
-        else:
-            print HINT_TEXT
-            sys.exit()
+    elif date_validator(input_value) is not False:
+        date = date_validator(input_value)
+    else:
+        street = input_value
 
     if not street_validator(street):
-        print HINT_TEXT
-        sys.exit()
-    if not date_validator(date):
-        print HINT_TEXT
+        print(HINT_TEXT)
         sys.exit()
 
     xml_element = xml_download(XML_PATH)
@@ -136,7 +127,7 @@ def main(argv):
     else:
         lista = search_events(xml_to_cityevents(xml_element), date, street)
         if len(lista) == 0:
-            print RED_COLOR_TAG + "Nothing found." + CLOSE_TAG
+            print(RED_COLOR_TAG + "Nothing found." + CLOSE_TAG)
             sys.exit()
         else:
             for i in lista:
@@ -144,4 +135,4 @@ def main(argv):
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
